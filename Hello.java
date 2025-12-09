2b5ae53fd61555ecb0be8b0bad38312b00d735e2
Syakiroh Suhaimah Agustin <syakirohhima@gmail.com>
2025-12-09 20:11:34 +0700

    Refactor: tambah komentar untuk penjelasan

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
+        //Menampilkan pesan Hello, ini mata kuliah java
         System.out.println("Hello, ini mata kuliah java");
     }
 }
