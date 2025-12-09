15f2bad8e7cee914aafbe38500ac4ec64c741901
Syakiroh Suhaimah Agustin <syakirohhima@gmail.com>
2025-12-09 20:14:59 +0700

    Enchance: tambah baris output kedua

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         //Menampilkan pesan Hello, ini mata kuliah java
         System.out.println("Hello, ini mata kuliah java");
+        System.out.println("Semangat Belajar!")
     }
 }
